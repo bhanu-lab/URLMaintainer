5b4d3f6eaf987607bb0178658bc07ce478e84b6b
bhanu-lab <bhanuputtareddy@gmail.com>
2021-05-22 02:05:53 +0530

    changed to use httServeMux to handle all connections

--- config/configreader.go
+++ config/configreader.go
@@ -3,6 +3,7 @@ package config
 import (
 	"io/ioutil"
 	"log"
+	"net/http"
 
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
@@ -18,20 +19,27 @@ type Mapper struct {
 }
 
 var (
-	logger *zap.Logger
-	err    error
+	logger       *zap.Logger
+	err          error
+	PathToURLMap URLMapper
 )
+
+type URLMapper map[string]string
 
 func init() {
 	logger, err = zap.NewDevelopment()
 	if err != nil {
 		log.Fatal("unable to initialize logger")
 	}
+	PathToURLMap = make(map[string]string)
+	PathToURLMap["mail"] = "https://mail.google.com/mail/u/0/#inbox"
 }
 
-// YamlHandler reads yaml file and retuns struct
-func YamlHandler() (*Config, error) {
-	buf, err := ioutil.ReadFile("config.yaml")
+// YamlHandler parses yaml file and unmarshalls
+// yaml bytes into struct defined
+func YamlHandler(yamlFilePath string) (*Config, error) {
+	logger.Info("yamlFilePath is ", zap.String("FilePath", yamlFilePath))
+	buf, err := ioutil.ReadFile(yamlFilePath)
 
 	if err != nil {
 		log.Fatal("error while reading file")
@@ -50,13 +58,16 @@ func YamlHandler() (*Config, error) {
 	return config, nil
 }
 
-// YamlMapBuilder builds a map with path and URL mapping from yaml file
-func YamlMapBuilder() map[string]string {
-	config, err := YamlHandler()
+// YamlMapBuilder builds a map from data
+// present in yaml file as Path and related
+// URL to be redirected to. The map created
+// is used to fetch URL based on path
+func YamlMapBuilder(yamlFilePath string) map[string]string {
+	config, err := YamlHandler(yamlFilePath)
 	configMap := make(map[string]string)
 
 	if err != nil {
-		logger.Warn("couldnt read data from yaml file")
+		logger.Fatal("couldnt read data from yaml file")
 		return configMap
 	}
 
@@ -67,4 +78,41 @@ func YamlMapBuilder() map[string]string {
 	}
 	return configMap
 
+}
+
+// URLRedirectMW middleware function which
+// acts on request and redirects URL
+// to another URL when it sees path matching
+// another URL defined in yaml file
+func URLRedirectMW(next http.Handler, yamlFilePath string) http.HandlerFunc {
+
+	yamlMap := YamlMapBuilder(yamlFilePath)
+	// combining 2 different map configs from map built from yaml file and local map config
+	PathToURLMap := FuseMap(PathToURLMap, yamlMap)
+	logger.Info("Map fused")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("received a request")
+		app := r.URL.Path
+		if redirectURL, ok := PathToURLMap[app]; ok {
+			logger.Info("URL is redirecting to %s since received app is %s \n", zap.String("redirectURL", redirectURL), zap.String("app", app))
+			http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
+		} else {
+			logger.Info("no mapping defined for \n", zap.String("app", app))
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// FuseMap combines 2 maps. m1 & m2 into
+// single map key values present in m2 will
+// over write key value pair from m1 map
+func FuseMap(m1, m2 URLMapper) URLMapper {
+	for key, value := range m1 {
+		if _, ok := m2[key]; !ok {
+			m2[key] = value
+		}
+	}
+	return m2
 }
--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"URLMaintainer/config"
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 )
 
@@ -17,67 +15,28 @@ var (
 
 type URLMapper map[string]string
 
-var PathToURLMap URLMapper
-
 func init() {
 	logger, err = zap.NewDevelopment()
 	if err != nil {
 		log.Fatal("unable to initialize logger")
 	}
-	PathToURLMap = make(map[string]string)
-	PathToURLMap["mail"] = "https://mail.google.com/mail/u/0/#inbox"
+}
+
+func getDefaultMux() *http.ServeMux {
+
+	mux := http.NewServeMux()
+	logger.Info("creating a new router")
+	mux.HandleFunc("/", Test)
+	return mux
 }
 
 func main() {
-	r := mux.NewRouter()
-	fmt.Println("creating a new router")
-	r.Use(URLRedirectMW)
-	fmt.Println("registerred middle ware")
-	r.HandleFunc("/{app}", Test)
-	http.ListenAndServe(":8082", r)
-}
-
-// URLRedirectMW middleware function which acts on request and redirects URL
-// to another URL when it sees path matching another URL defined in yaml file
-func URLRedirectMW(next http.Handler) http.Handler {
-
-	// combining 2 different map configs from map built from yaml file and local map config
-	PathToURLMap := FuseMap(PathToURLMap, config.YamlMapBuilder())
-	logger.Info("Map fused")
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("received a request")
-		vars := mux.Vars(r)
-		app := vars["app"]
-		if redirectURL, ok := PathToURLMap[app]; ok {
-			logger.Info("URL is redirecting to %s since received app is %s \n", zap.String("redirectURL", redirectURL), zap.String("app", app))
-			http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
-		} else {
-			logger.Info("no mapping defined for app %s \n", zap.String("app", app))
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func AppRouter(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	r := getDefaultMux()
+	mapHandler := config.URLRedirectMW(r, "config.yaml")
+	http.ListenAndServe(":8082", mapHandler)
 }
 
 // Test function to test
 func Test(w http.ResponseWriter, r *http.Request) {
 	logger.Info("received a test request. Its working!!")
-}
-
-// FuseMap combines 2 maps. If both m1, m2 has same keys m2 is retained
-func FuseMap(m1, m2 URLMapper) URLMapper {
-	for key, value := range m1 {
-		if _, ok := m2[key]; !ok {
-			m2[key] = value
-		}
-	}
-	fmt.Printf("Final combined map %+v \n", m2)
-	return m2
 }
